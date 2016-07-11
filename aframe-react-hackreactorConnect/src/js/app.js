afbca7e0eb4dd59cc615ef5a9e423e222e5ed2c0
Christopher Pruijsen <christopher.pruijsen@gmail.com>
2016-07-11 13:12:17 -0700

    Rendering all cohorts dynamically with animation

--- aframe-react-hackreactorConnect/src/js/app.js
+++ aframe-react-hackreactorConnect/src/js/app.js
@@ -43,13 +43,13 @@ class BoilerplateScene extends React.Component {
   });
 };
 
-  changeCohort = () => {
-    if (this.state.cohort < 2) {
-      this.setState({cohort: this.state.cohort + 1});
-    } else {
-      this.setState({cohort: 0});
-    }
-  };
+  // changeCohort = () => {
+  //   if (this.state.cohort < 2) {
+  //     this.setState({cohort: this.state.cohort + 1});
+  //   } else {
+  //     this.setState({cohort: 0});
+  //   }
+  // };
 
   getLayoutOptions = () => {
     if (this.state.layout === 'circle') {
@@ -92,25 +92,67 @@ class BoilerplateScene extends React.Component {
   };
 
   render () {
-
+    // general internal
     var that = this;
-    // layout shapes demo
     var currentLayoutOptions = that.getLayoutOptions();
 
-    // general internal
-    var len = data.length;
-    var datasq = Math.floor(Math.sqrt(len)); // 43 => 6
-    var datacb = Math.floor(Math.cbrt(len));
-    var numCircles = Math.floor(len / 10); // arbitrary... for now.
-    var dataRangeToSqrt = _.range(0, datasq + 1); // [0, 1, 2, 3, 4, 5, 6]
-    var dataRangeToCbrt = _.range(0, datacb ); // 43 => [0, 1, 2, 3]
-    var dataRangeCircles = _.range(0, numCircles); // 42 => [0, 1, 2, 3]
+    // function to create an object which holds each cohort as a separate object, which includes functions and variables dependent on the length of that cohorts users array.
+    // these are in turn used for dynamic layout and rendering.
+    // TODO: refactor and import this from another file.
+    var createDataMapObj = function() {
+      var obj = {};
+      var len = data.length;
+      for (let i = 0; i < len; i++) {
+        obj[i] = data[i];
+        obj[i].len = obj[i].users.length;
+        obj[i].datasq = Math.floor(Math.sqrt(obj[i].len));
+        obj[i].datacb = Math.floor(Math.cbrt(obj[i].len));
+        obj[i].numCircles = Math.floor(obj[i].len / 10);
+        obj[i].dataRangeCircles = _.range(0, obj[i].numCircles);
+        obj[i].circleIterator = obj[i].numCircles;
+        obj[i].circleSliceStart = -10;
+        obj[i].circleSliceEnd = 0;
+        obj[i].circlePosition = 0;
+        obj[i].changePosForCylinder = function() {
+          this.circleIterator--;
+          var z = i * 10 + 10; // so each dataset has a unique rendering position for the cylinders
+          var position = this.dataRangeCircles[this.circleIterator];
+          this.circlePosition = `0 ${position} ${z}`;
+        };
+        obj[i].determineSlicingCylinder = function(options, i) {
+          if (options.all) { // renders remaining people
+            this.circleSliceStart += 10;
+            if (i === this.numCircles - 1) {
+              // this makes for very ugly cylinders.
+              this.circleSliceEnd = this.circleSliceStart + 10 + (this.len % 10);
+            } else {
+              this.circleSliceEnd = this.circleSliceStart + 10;
+            }
+          } else {
+            this.circleSliceStart += 10;
+            this.circleSliceEnd = this.circleSliceStart + 10;
+          }
+        };
+      }
+      return obj;
+    };
+    var datamap = createDataMapObj();
+    // {0: { }, 1: {}, 2: {}}
+    // where 0:{group_name, group_id, users, len, ... }
+
+    // var len = data.length;
+    // var datasq = Math.floor(Math.sqrt(len)); // 43 => 6
+    // var datacb = Math.floor(Math.cbrt(len));
+    // var dataRangeToSqrt = _.range(0, datasq + 1); // [0, 1, 2, 3, 4, 5, 6]
+    // var dataRangeToCbrt = _.range(0, datacb ); // 43 => [0, 1, 2, 3]
+
+    // NOTE: not so functional atm. dynamic data switch.
     var cohort = data[that.state.cohort].users;
 
     return (
-      // TODO: make physics work
-      // kinematic-body on camera, static body on floor and Sky
-      // physics on Scene
+      // TODO: make physics work w/ jump and collision
+      // `kinematic-body` on camera, `static-body` on floor and Sky
+      // `physics` on Scene
 
       <Scene>
         <Camera><Cursor/></Camera>
@@ -121,11 +163,87 @@ class BoilerplateScene extends React.Component {
         <Entity light={{type: 'directional', intensity: 0.5}} position={[-1, 1, 0]}/>
         <Entity light={{type: 'directional', intensity: 1}} position={[1, 1, 0]}/>
 
+        {datamap[0].dataRangeCircles.map(function(i) {
+          datamap[0].changePosForCylinder();
+          datamap[0].determineSlicingCylinder({all: true}, i);
+
+          return <Entity layout={{type: 'circle', radius: `${datamap[0].datacb}`}} position={datamap[0].circlePosition}>
+          <Animation attribute="layout.radius" repeat="indefinite" to={`${datamap[0].datasq}`} direction="alternate" begin="5000"/>
+
+          {datamap[0].users.slice(datamap[0].circleSliceStart, datamap[0].circleSliceEnd).map(function(person) {
+
+            return <Entity key={person.id} data={person}
+                geometry="primitive: box"
+                material={{src: `url(${person.image})`, color: that.state.color}}
+                onClick={that.changeColor} >
+                {/*<Entity text={`text:  ${person.name}`}
+                        material="color: #66E1B4"
+                        scale="0.3 0.3 0.3"
+                        position="0 .5 -1"
+                        rotation="0 180 0"
+                        visible="true"
+                </Entity>*/}
+              </Entity>; })}
+            </Entity> })}
+
+          {datamap[1].dataRangeCircles.map(function(i) {
+            datamap[1].changePosForCylinder();
+            datamap[1].determineSlicingCylinder({all: true}, i);
+
+            return <Entity layout={{type: 'circle', radius: `${datamap[1].datacb}`}} position={datamap[1].circlePosition}>
+            <Animation attribute="layout.radius" repeat="indefinite" to={`${datamap[1].datasq}`} direction="alternate" begin="5000"/>
+
+            {datamap[1].users.slice(datamap[1].circleSliceStart, datamap[1].circleSliceEnd).map(function(person) {
+
+              return <Entity key={person.id} data={person}
+                  geometry="primitive: box"
+                  material={{src: `url(${person.image})`, color: that.state.color}}
+                  onClick={that.changeColor} >
+                  {/*<Entity text={`text:  ${person.name}`}
+                          material="color: #66E1B4"
+                          scale="0.3 0.3 0.3"
+                          position="0 .5 -1"
+                          rotation="0 180 0"
+                          visible="true" </Entity>*/}
+                </Entity>; })}
+              </Entity> })}
+
+            {datamap[2].dataRangeCircles.map(function(i) {
+              datamap[2].changePosForCylinder();
+              datamap[2].determineSlicingCylinder({all: true}, i);
+
+              return <Entity layout={{type: 'circle', radius: `${datamap[2].datacb}`}} position={datamap[2].circlePosition}>
+              <Animation attribute="layout.radius" repeat="indefinite" to={`${datamap[2].datasq}`} direction="alternate" begin="5000"/>
+
+              {datamap[2].users.slice(datamap[2].circleSliceStart, datamap[2].circleSliceEnd).map(function(person) {
+
+                return <Entity key={person.id} data={person}
+                    geometry="primitive: box"
+                    material={{src: `url(${person.image})`, color: that.state.color}}
+                    onClick={that.changeColor} >
+                    {/*<Entity text={`text:  ${person.name}`}
+                            material="color: #66E1B4"
+                            scale="0.3 0.3 0.3"
+                            position="0 .5 -1"
+                            rotation="0 180 0"
+                            visible="true" </Entity>*/}
+                  </Entity>; })}
+                </Entity> })}
+
+
+
+
         {/* changeLayout control entity for layout test container */}
         <Entity onClick={that.changeLayout} geometry="primitive: cylinder" material="color: red" position="-10 0 1"> </Entity>
 
-        {/* changeCohort control entity for layout test container */}
-        <Entity onClick={that.changeCohort} geometry="primitive: cylinder" material="color: green" position="-13 0 1"> </Entity>
+        {/* changeCohort control entity for layout test container
+          TODO: need to debug.
+          layout on change of data renders a new layout
+          but this new layout cannot 'overlap' the old one
+          so it might be better to instead remove and add a new layout
+
+          <Entity onClick={that.changeCohort} geometry="primitive: cylinder" material="color: green" position="-13 0 1"> </Entity>
+        */}
 
         {/* Layout tests container entity component */}
           <Entity
@@ -133,15 +251,15 @@ class BoilerplateScene extends React.Component {
               margin: `${currentLayoutOptions.margin}`,
               radius: `${currentLayoutOptions.radius}`,
               columns: `${currentLayoutOptions.columns}` }}
-              position="0 1 0" >
+              position="20 0 -10" >
 
           {cohort.map(function(person) {
             return <Entity key={person.id} data={person}
-                    static-body="shape: box"
+                    // static-body="shape: box"
                     geometry="primitive: box"
                     material={{src: `url(${person.image})`, color: that.state.color}}
                     onClick={that.changeColor} >
-              {/*<Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>*/}
+              <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
             </Entity>; })}
           </Entity>
 
